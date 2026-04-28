3b747b4188b9228bfd1a9f00d7a55173485e380d
Supratimdas20 <supratimdaswork@gmail.com>
2026-04-28 23:36:59 +0530

    Learning Locators.

--- Selenium/src/test/java/day1/FirstTestCase.java
+++ Selenium/src/test/java/day1/FirstTestCase.java
@@ -1,10 +1,23 @@
 package day1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FirstTestCase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing....");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://automationexercise.com");
+		String actTitle=driver.getTitle();
+		if(actTitle.equals("Automation Exercise")) {
+			
+			System.out.println("Test Passed");
+		}
+		else {
+			System.out.println("Test Failed");
+		}
+		driver.close();
 
 	}
 
---
+++ Selenium/src/test/java/day2/Locators.java
@@ -0,0 +1,20 @@
+package day2;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.nopcommerce.com/");
+		List<WebElement> searchBox=driver.findElements(By.id("small-searchterms"));;
+
+	}
+
+}
